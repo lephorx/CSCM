485885cc8c64ba9f9624f1ff956f541aa1ca0169
lephorx <ramonzielke09@gmail.com>
2026-04-20 23:29:04 +0200

    fix crafty issue

--- server_manager.py
+++ server_manager.py
@@ -157,6 +157,7 @@ def provision_server(
                 "mem_min": mem_min,
                 "mem_max": mem_max,
                 "server_properties_port": server_port,
+                "executable_update_url": "",
             },
         },
     }
